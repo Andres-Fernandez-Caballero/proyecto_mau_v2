621330e6226d2ddc94bb71cac57acf0d45a98679
Andres <andres.fernandezcaballero@davinci.edu.ar>
2021-04-12 19:00:08 -0300

    Revert "Alpha_V1.1.1"
    This reverts commit 18531530766fbe9936362cddf2c886cf1ca2f7fa.

--- resources/views/layouts/wellcome_app.blade.php
+++ resources/views/layouts/wellcome_app.blade.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
-<html lang="es">
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
     <head>
-        <meta charset="UTF-8">
+        <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
         <!-- CSRF Token -->
@@ -11,14 +11,19 @@
         <title>{{config("app.name")}}</title>
 
 
+        <!-- Scripts -->
+        <script src="{{ asset('js/app.js') }}" defer></script>
+
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
 
         <!-- Styles -->
-        <link href="{{ asset('css/wellcome.css') }}" rel="stylesheet">    
+        <link rel="stylesheet" href="{{ asset('css/wellcome.css') }}">
+        <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+        
     </head>
-    <body>
-        <div>
+    <body class="antialiased">
+        <div id="app">
             <main>
                 @yield('content')
             </main>
